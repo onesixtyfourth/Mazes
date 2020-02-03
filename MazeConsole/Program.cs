fd9b95f289c778a086a69e9219c16038197d903c
Leon GLASS <leon.glass@aldautomotive.com>
2020-02-03 12:05:20 +0000

    More tidying

--- MazeConsole/Program.cs
+++ MazeConsole/Program.cs
@@ -1,15 +1,8 @@
-﻿using System;
-using System.Linq;
-using MazeLib.Factory;
-using MazeLib.Solvers;
-using MazeLib.Utilities;
-using Microsoft.Extensions.Hosting;
+﻿using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Serilog;
 using System.Threading.Tasks;
-
-using System.Collections.Generic;
 
 namespace MazeConsole
 {
@@ -23,17 +16,17 @@ namespace MazeConsole
             await console.Run();
         }
 
-         public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
-                .UseSerilog()
-                .ConfigureAppConfiguration((hostingContext, config) =>
-                {
-                    config.AddJsonFile("settings.json", true, true);
-                    config.AddCommandLine(args);
-                })
-                .ConfigureServices((hostcontext, services) =>
-                {
-                    services.AddTransient<ConsoleApp>();
-                });
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+           Host.CreateDefaultBuilder(args)
+               .UseSerilog()
+               .ConfigureAppConfiguration((hostingContext, config) =>
+               {
+                   config.AddJsonFile("settings.json", true, true);
+                   config.AddCommandLine(args);
+               })
+               .ConfigureServices((hostcontext, services) =>
+               {
+                   services.AddTransient<ConsoleApp>();
+               });
     }
 }
